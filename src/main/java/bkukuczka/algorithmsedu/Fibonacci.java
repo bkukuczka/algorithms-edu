db73bd9a7fbb3ed8d5a837a0c193707ba27c5c0d
Bartłomiej Kukuczka <kukuczkabartlomiej@gmail.com>
2024-04-10 11:20:56 +0200

    fibonacci

---
+++ src/main/java/bkukuczka/algorithmsedu/Fibonacci.java
@@ -0,0 +1,24 @@
+package bkukuczka.algorithmsedu;
+
+class Fibonacci {
+    long fib(int n) {
+
+        if (n <= 2) {
+            return n;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    long fibNoRec(int n) {
+        long[] arr = new long[n];
+        arr[0] = 1L;
+        arr[1] = 2L;
+
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        return arr[n-1];
+    }
+}
