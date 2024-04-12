10602f927fe31d2ebdfb4cf817441d9f9a125761
Bartłomiej Kukuczka <kukuczkabartlomiej@gmail.com>
2024-04-12 19:45:44 +0200

    is string a palindrome skipping punctuation

---
+++ src/test/java/bkukuczka/algorithmsedu/leetcode/IsPalindromeTest.java
@@ -0,0 +1,30 @@
+package bkukuczka.algorithmsedu.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IsPalindromeTest {
+    public static Stream<Arguments> isPalindromeTestData() {
+        return Stream.of(
+                Arguments.of("A man, a plan, a canal: Panama", true),
+                Arguments.of("racecar", true),
+                Arguments.of("race a car", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("isPalindromeTestData")
+    void isStringAPalindrome(String s, boolean expectedResult) {
+        //when
+        var result = new IsPalindrome().isPalindrome(s);
+
+        //then
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+}
