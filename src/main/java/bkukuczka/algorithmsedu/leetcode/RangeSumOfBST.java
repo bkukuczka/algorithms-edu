348f1adb30c0d6441946d3483bde8a49d8cd7f09
Bartłomiej Kukuczka <kukuczkabartlomiej@gmail.com>
2024-04-15 18:37:23 +0200

    range sum in BST (dfs)

---
+++ src/main/java/bkukuczka/algorithmsedu/leetcode/RangeSumOfBST.java
@@ -0,0 +1,45 @@
+package bkukuczka.algorithmsedu.leetcode;
+
+import bkukuczka.algorithmsedu.commons.BinaryTreeNode;
+
+import java.util.Stack;
+
+class RangeSumOfBST {
+    int rangeSum(BinaryTreeNode<Integer> root, int L, int R) {
+        int sum = 0;
+        Stack<BinaryTreeNode<Integer>> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            var node = stack.pop();
+
+            if (node != null) {
+                if (node.value >= L && node.value <= R) {
+                    sum += node.value;
+                }
+
+                if (node.value > L) {
+                    stack.push(node.left);
+                }
+
+                if (node.value < R) {
+                    stack.push(node.right);
+                }
+            }
+        }
+
+        return sum;
+
+//        if (root == null) {
+//            return 0;
+//        }
+//
+//        int sum = 0;
+//
+//        if (root.value >= L && root.value <= R) {
+//            sum = root.value;
+//        }
+//
+//        return sum + rangeSum(root.left, L, R) + rangeSum(root.right, L, R);
+    }
+}
---
+++ src/test/java/bkukuczka/algorithmsedu/leetcode/RangeSumOfBSTTest.java
@@ -0,0 +1,26 @@
+package bkukuczka.algorithmsedu.leetcode;
+
+import bkukuczka.algorithmsedu.commons.BinaryTreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RangeSumOfBSTTest {
+    @Test
+    void shouldReturnSumOfNodesBetweenLAndR() {
+        //given
+        var node7 = new BinaryTreeNode<>(18, null, null);  //right-right
+        var node5 = new BinaryTreeNode<>(7, null, null);  //left-right
+        var node4 = new BinaryTreeNode<>(3, null, null); //left-left
+        var node3 = new BinaryTreeNode<>(15, null, node7); //right
+        var node2 = new BinaryTreeNode<>(5, node4, node5); //left
+        var node1 = new BinaryTreeNode<>(10, node2, node3); //root
+
+        //when
+        var result = new RangeSumOfBST().rangeSum(node1, 7, 15);
+
+        //then
+        assertThat(result).isEqualTo(32);
+    }
+
+}
