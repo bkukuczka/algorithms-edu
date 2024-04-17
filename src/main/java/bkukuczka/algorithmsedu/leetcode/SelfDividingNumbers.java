82e9fa61231e2a3957445845a7b50f6a1e6387cd
Bartłomiej Kukuczka <kukuczkabartlomiej@gmail.com>
2024-04-17 21:39:15 +0200

    self dividing numbers

---
+++ src/main/java/bkukuczka/algorithmsedu/leetcode/SelfDividingNumbers.java
@@ -0,0 +1,22 @@
+package bkukuczka.algorithmsedu.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SelfDividingNumbers {
+    List<Integer> findNumbers(int left, int right) {
+        List<Integer> result = new ArrayList<>();
+        outer:
+        for (int i = left; i <= right; i++) {
+
+            for (int j = i; j > 0; j /= 10) {
+                if (j % 10 == 0 || i % (j % 10) != 0) {
+                    continue outer;
+                }
+            }
+            result.add(i);
+        }
+
+        return result;
+    }
+}
