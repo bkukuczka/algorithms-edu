3a0baadfb3f1500c3038a2c990c55eb4e4ebebf9
Bartłomiej Kukuczka <kukuczkabartlomiej@gmail.com>
2024-04-18 11:44:19 +0200

    add two numbers stored in reverse in linked lists

---
+++ src/main/java/bkukuczka/algorithmsedu/leetcode/AddTwoNumbersLinkedLists.java
@@ -0,0 +1,29 @@
+package bkukuczka.algorithmsedu.leetcode;
+
+import bkukuczka.algorithmsedu.commons.ListNode;
+
+class AddTwoNumbersLinkedLists {
+    ListNode<Integer> add(ListNode<Integer> headOne, ListNode<Integer> headTwo) {
+        var resultHead = new ListNode<>(0, null);
+        var current = resultHead;
+
+        int decimalNumber = 0;
+
+        while (headOne != null && headTwo != null) {
+            int sum = headOne.value + headTwo.value + decimalNumber;
+            decimalNumber = sum / 10;
+            int remainder = sum % 10;
+
+            current.next = new ListNode<>(remainder, null);
+            current = current.next;
+            headOne = headOne.next;
+            headTwo = headTwo.next;
+        }
+
+        if (decimalNumber > 0) {
+            current.next = new ListNode<>(decimalNumber, null);
+        }
+
+        return resultHead.next;
+    }
+}
