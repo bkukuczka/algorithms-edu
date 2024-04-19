e2100df5842014815ae8aa5beb43a007a328bef1
Bartłomiej Kukuczka <kukuczkabartlomiej@gmail.com>
2024-04-19 22:20:47 +0200

    min subarray size for sum - sliding window

---
+++ src/main/java/bkukuczka/algorithmsedu/leetcode/MinimumSizeSubarraySum.java
@@ -0,0 +1,29 @@
+package bkukuczka.algorithmsedu.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MinimumSizeSubarraySum {
+    int minSubArrLen(int s, int[] nums) {
+        Queue<Integer> window = new LinkedList<>();
+        int currentSum = 0;
+        int minSize = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            window.offer(i);
+            currentSum += i;
+
+            if (currentSum >= s) {
+                minSize = Math.min(minSize, window.size());
+                currentSum = currentSum - window.poll();
+
+                while (currentSum >= s && !window.isEmpty()) {
+                    minSize = Math.min(minSize, window.size());
+                    currentSum = currentSum - window.poll();
+                }
+            }
+        }
+
+        return minSize;
+    }
+}
